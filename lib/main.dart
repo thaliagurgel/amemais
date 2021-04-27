e084ff9ef790c6c2bdd6e4e6ad718808ee2ef416
Thalia Gurgel <thaliasampaio8@gmail.com>
2021-04-26 22:03:08 -0300

    campo repetir senha

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: AmeMaisColors.branco,
+        fontFamily: "Muli",
+        textTheme: TextTheme(bodyText1: TextStyle(color: AmeMaisColors.branco)),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: WelcomeScreen(),
     );
